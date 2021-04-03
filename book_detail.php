625f6e7efb73de2d3ddc517868cbf179e6697a64
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-04-03 23:21:49 +0200

    added book details page

--- book_detail.php
+++ book_detail.php
@@ -11,6 +11,7 @@ if (!isset($_GET['ISBN']) || $_GET['ISBN'] == "")
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/book_detail.css">
     <title>Biblioteca facile!</title>
 </head>
 
@@ -19,7 +20,209 @@ if (!isset($_GET['ISBN']) || $_GET['ISBN'] == "")
     <div class="content">
         <?php
         require_once('assets/header.php');
+
+        //cerco dati libro da mostrare
+        //preparo query di ricerca
+        $ISBN = htmlspecialchars($_GET['ISBN']);
+        $query = "SELECT BOOK.ISBN, title, subtitle, language, year, cover, name AS publisher, publisher.idPublisher AS idPublisher, COUNT(copy.idCopy) AS copyNumber
+                FROM book  
+                INNER JOIN publisher ON book.idPublisher = publisher.idPublisher
+                /* join con copie NON in prestito */
+                LEFT JOIN copy ON book.ISBN = copy.ISBN 
+                                AND copy.idCopy NOT IN (SELECT borrow.idCopy 
+                                                        FROM loan, borrow
+                                                        WHERE returnDate IS NULL)
+                /* controllo ISBN */
+                WHERE book.ISBN = :ISBN
+                GROUP BY book.ISBN";
+
+        //array di valori da passare
+        $values = array(':ISBN' => $ISBN);
+
+
+        /* esecuzione query */
+        try {
+            //prepare query
+            $res = $pdo->prepare($query);
+
+            //esecuzione con passaggio di valori di ricerca
+            $res->execute($values);
+        } catch (PDOException $e) {
+
+            //in caso di errore stampo con stile
+            echo "<div class=\"alert alert-danger\">
+                    <strong>Errore!</strong> Errore nella ricerca
+                </div>";
+            var_dump($e);
+        }
+
+        echo "<br>";
+
+        //controllo se esiste il libro
+        if ($res->rowCount() == 1) {
+
+            //fetch
+            $book = $res->fetch();
+
+            /* ricerca degli autori */
+
+            //query per trovare autori
+            $query = "SELECT author.idAuthor, name, surname
+            FROM author, write_book AS wb
+            WHERE author.idAuthor = wb.idAuthor
+                AND wb.ISBN = :ISBN
+            ORDER BY wb.position;";
+
+
+
+            //array di valori da passare per la query autori
+            $values = array(':ISBN' => $ISBN);
+
+            /* esecuzione query */
+            try {
+                //prepare query
+                $res = $pdo->prepare($query);
+
+                //esecuzione con passaggio di valori di ricerca
+                $res->execute($values);
+
+                //fetch
+                $autori = $res->fetchAll();
+            } catch (PDOException $e) {
+                echo $e;
+
+                //in caso di errore stampo con stile
+                echo "<div class=\"alert alert-danger\">
+                        <strong>Errore!</strong> Errore nella ricerca
+                        </div>";
+                die();
+            }
+
         ?>
+            <!-- stampa dettaglio -->
+            <div class="book">
+                <!-- cover  -->
+                <div class="left">
+                    <?php
+                    echo '<img id="cover" src="images/';
+                    if ($book['cover'] != NULL) {
+                        echo $book['cover'];
+                    } else {
+                        echo "no-image.jpg";
+                    }
+                    echo '"/>';
+                    ?>
+                </div>
+
+                <!-- dati testutali -->
+                <div class="right">
+                    <table id="details">
+                        <tr>
+                            <td class="property">
+                                ISBN:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo $ISBN;
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Titolo:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['title']);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Sottotitolo:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['subtitle']);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Autori:
+                            </td>
+                            <td class="content">
+                                <?php
+                                //stampo gli autori utilizzando l'index per capire se devo mettere la virgola (o se è l'ultimo)
+                                foreach ($autori as $index => $autore) {
+                                    echo $autore['name'] . " " . $autore['surname'];
+
+                                    if (count($autori) > $index + 1) {
+                                        echo ", ";
+                                    }
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Lingua:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['language']);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Anno:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['year']);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Editore:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['publisher']);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="property">
+                                Copie disponibili:
+                            </td>
+                            <td class="content">
+                                <?php
+                                echo htmlspecialchars($book['copyNumber']);
+                                ?>
+                            </td>
+                        </tr>
+                    </table>
+
+                </div>
+            </div>
+
+
+        <?php
+        } else {
+
+            //in caso non ci siano titoli stampo un info
+            echo "<div class=\"alert alert-info\">
+                <strong>Attenzione!</strong> Nessun titolo trovato.
+                </div>";
+        }
+        ?>
+
+
+
+
     </div>
 
 </body>
