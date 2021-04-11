7ded574050e44434bd9d5cc66c4a0dd5b12c2ec0
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-04-11 22:35:35 +0200

    login in header

--- assets/header.php
+++ assets/header.php
@@ -8,24 +8,43 @@
 <body>
 
     <?php
+    //require del login e del db
     require_once("db.php");
+    require_once("check_login.php");
     ?>
-    
+
     <header>
 
+        <!-- logo -->
         <a href="/" class="logo">
             <img class="logo" src="img/logo.jpg" alt="Biblioteca Facile" />
         </a>
+
+        <!-- menu di navigazione -->
         <div class="right">
+
+            <!-- div up dedicato al login/gestione utente -->
             <div id="up">
-                <a href="login.php">Accedi o registrati</a>
+                <?php
+                //controllo autenticazione dell'utente e in caso metto i dati
+                if ($account->isAuthenticated()) {
+                    echo "ciao " . $account->getName();
+                } else {
+                ?>
+                    <a href="login.php">Accedi o registrati</a>
+                <?php
+                }
+                ?>
                 <!-- Benvenuto tizio! -->
             </div>
+
+            <!-- div down per nav tra le pagine -->
             <div id="down">
                 <?php
+                //controlla la pagina in cui si è
                 $fileName = basename($_SERVER['PHP_SELF']);
 
-
+                //se sono nelle pagine di un nav evidenzio la casella                
                 if ($fileName == "index.php") {
                     echo '<a class="active" href="/">Catalogo</a>';
                 } else {
