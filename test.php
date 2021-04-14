2d900b9ea3a73c26c637602198cd05dc1cd9a9f6
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-04-14 22:56:13 +0200

    Modifiche complementari alla gestione utente

--- test.php
+++ test.php
@@ -3,19 +3,26 @@ session_start();
 include_once("classes/user_class.php");
 $account = new User();
 
+
 $login = FALSE;
 
-try {
-    $login = $account->sessionLogin();
-} catch (Exception $e) {
-    echo $e->getMessage();
-    die();
+try
+{
+	$login = $account->login('toninelli.alessandro00@gmail.com', 'Stallman00');
+}
+catch (Exception $e)
+{
+	echo $e->getMessage();
+	die();
 }
 
-if ($login) {
-    echo 'Authentication successful.';
-    echo 'Account ID: ' . $account->getId() . '<br>';
-    echo 'Account name: ' . $account->getName() . '<br>';
-} else {
-    echo 'Authentication failed.';
+if ($login)
+{
+	echo 'Authentication successful.';
+	echo 'Account ID: ' . $account->getId() . '<br>';
+	echo 'Account name: ' . $account->getMail() . '<br>';
+}
+else
+{
+	echo 'Authentication failed.';
 }
