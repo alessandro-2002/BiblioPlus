a65384100d73471e7c9b021426407bced577185c
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-04-30 22:35:58 +0200

    aggiunta admin area

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once("../assets/admin_session_login.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Area Bibliotecario</title>
+</head>
+
+<body>
+    <?php
+    var_dump($adminAccount);
+    ?>
+
+
+</body>
+
+</html>
