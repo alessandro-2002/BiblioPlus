c411c4b9eed372700ff6d7f00ef6ce2514e8693e
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-05-01 21:40:42 +0200

    Update index.php
    Aggiunta header in index e titolo

--- admin/index.php
+++ admin/index.php
@@ -1,21 +1,22 @@
-<?php
-require_once("../assets/admin_session_login.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="it">
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/base.css">
     <title>Area Bibliotecario</title>
 </head>
 
 <body>
-    <?php
-    var_dump($adminAccount);
-    ?>
+    <div class="content">
+        <?php
+        require_once("../assets/admin_header.php");
+        ?>
 
+        <h1>Dashboard Bibliotecario</h1>
+
+        <a href="users.php">Gestione Utenti</a>
+    </div>
 
 </body>
 
