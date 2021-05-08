8dd9b21db3427d385ca08f6fed67788fa456b825
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-05-09 00:41:57 +0200

    Update admin_new_loan.php
    Inizio elaborazione (controllo dati) nuovo prestito da terminare e ottimizzare

--- admin/admin_new_loan.php
+++ admin/admin_new_loan.php
@@ -22,16 +22,103 @@
     <div class="content">
         <?php
         require_once("../assets/admin_header.php");
+
+        //controllo ACL e se non le ha non lascio l'accesso alla pagina, stampo errore
+        if ($adminAccount->getACLloan()) {
+
+            //controllo se in post sta inserendo prestito
+            if (isset($_POST['newLoan'])) {
+                //controllo siano stati inseriti i dati
+                if (isset($_POST['idUser']) && isset($_POST['duration']) && isset($_POST['idCopy'])) {
+
+                    //controlli per la correttezza dei dati inseriti, lavoro in un unico blocco try-catch
+                    try {
+                        /* controllo esistenza utente */
+
+                        //query di ricerca per utente esistente e abilitato
+                        $query = "SELECT idUser
+                                FROM user
+                                WHERE idUser = :idUser
+                                    AND isEnabled";
+
+                        //array di valori da passare
+                        $values = array(':idUser' => $_POST['idUser']);
+
+                        // esecuzione query 
+                        try {
+                            //prepare query
+                            $res = $pdo->prepare($query);
+
+                            //esecuzione con passaggio di valori
+                            $res->execute($values);
+                        } catch (PDOException $e) {
+                            throw new Exception("Query error");
+                        }
+
+                        //controllo se non c'è un utente, in tal caso lancio eccezione altrimenti continuo
+                        if ($res->rowCount() != 1) {
+                            throw new Exception("Utente inesistente o non abilitato.");
+                        }
+
+
+                        /* controllo esistenza e che le copie non siano in prestito */
+
+                        //query di ricerca base per copie esistenti e non in prestito
+                        $query = "SELECT idCopy
+                                FROM copy
+                                WHERE idCopy NOT IN (SELECT idCopy
+                                                    FROM borrow, loan
+                                                    WHERE borrow.idLoan = loan.idLoan
+                                                        AND loan.returnDate IS NULL)
+                                    AND idCopy = :idCopy";
+
+                                    //AGGIUNGERE NELLA QUERY LA LISTA DELLE COPIE CON idCopy IN (...)
+                                    //CONTROLLARE POI QUANTE COPIE VENGONO RESTITUITE E QUALI MANCANO RISPETTO A QUELLE INSERITE
+                                    //SI OTTIMIZZA CON UNA SOLA EXECUTE E SI OTTENGONO CONTEMPORANEAMENTE TUTTI GLI ERRORI
+
+                        //prepare query per ottimizzare esecuzione
+                        $res = $pdo->prepare($query);
+
+                        foreach ($_POST['idCopy'] as $idCopy) {
+                            //array di valori da passare
+                            $values = array(':idCopy' => $idCopy);
+
+                            // esecuzione query 
+                            try {
+                                //esecuzione con passaggio di valori
+                                $res->execute($values);
+                            } catch (PDOException $e) {
+                                throw new Exception("Query error");
+                            }
+
+                            //controllo se c'è un id, quindi la copia esiste ed è libera altrimenti genero eccezione
+                            if ($res->rowCount() != 1) {
+                                throw new Exception("Copia id:" . $idCopy . " inesistente o gi&agrave; in prestito.");
+                            }
+                        }
+                    } catch (Exception $e) {
+                        echo '<br><div class="alert alert-danger">
+                                <strong>Errore!</strong> ' . $e->getMessage() .
+                            '</div>';
+                    }
+
+                    //se non sono inseriti tutti i dati restituisco errore
+                } else {
+                    echo '<br><div class="alert alert-danger">
+                        <strong>Errore!</strong> Non hai inserito tutti i campi obbligatori.
+                    </div>';
+                }
+            }
         ?>
 
-        <div class="container">
-            <h1>Nuovo Prestito</h1>
+            <div class="container">
 
-            <hr>
+                <h1>Nuovo Prestito</h1>
 
-            <?php
-            //controllo ACL e se non le ha non lascio l'accesso alla pagina, stampo errore
-            if ($adminAccount->getACLloan()) {
+                <hr>
+
+                <?php
+
 
                 //get lista utenti abilitati per select utente
                 $query = "SELECT idUser, name, surname
@@ -50,7 +137,7 @@
                 }
 
                 $users = $res->fetchAll();
-            ?>
+                ?>
 
                 <form action="" method="POST">
                     <div class="form-group row">
@@ -105,24 +192,25 @@
 
                     <div class="form-group row">
                         <div class="offset-4 col-8">
-                            <button name="submit" type="submit" class="btn btn-primary">Inserisci</button>
+                            <button name="newLoan" type="submit" class="btn btn-primary">Inserisci</button>
                             <a href="" class="btn btn-default" role="button">Reset</a>
 
                         </div>
                     </div>
 
-
-                <?php
-
-
-            } else {
-                echo '<br><div class="alert alert-danger">
-                        <strong>Errore!</strong> Non disponi dell\'autorizzazione effettuare prestiti.
-                    </div>';
-            }
-                ?>
                 </form>
-        </div>
+            </div>
+        <?php
+
+
+        } else {
+            echo '<br><div class="alert alert-danger">
+                    <strong>Errore!</strong> Non disponi dell\'autorizzazione effettuare prestiti.
+                </div>';
+        }
+        ?>
+
+
     </div>
 
 </body>
