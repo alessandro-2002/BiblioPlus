5f015e6871da6a2a746d9feb01a2ab3481ab37a4
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-05-12 21:53:52 +0200

    Update login
    Update login bootstrap

--- login.php
+++ login.php
@@ -2,66 +2,67 @@
 <html>
 
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <title>Biblio+</title>
+
+    <title>Login</title>
 </head>
 
 <body>
-    <?php
+    <div class="login text-center">
+        <form action="" method="POST">
 
-    require_once("assets/db.php");
-    require_once('classes/user_class.php');
-    require_once('assets/session_login.php');
+            <?php
 
-    try {
-        //se è già connesso lo reindirizzo in home 
-        if ($login) {
-            header('Location: /');
+            require_once("assets/db.php");
+            require_once('classes/user_class.php');
+            require_once('assets/session_login.php');
 
-            //altrimenti controllo se sta facendo login in post
-        } else if (isset($_POST['mail']) && isset($_POST['password'])) {
-            $login = $account->login($_POST['mail'], $_POST['password']);
+            try {
+                //se è già connesso lo reindirizzo in home 
+                if ($login) {
+                    header('Location: /');
 
-            if ($login) {
-                echo '<br><div class="alert alert-success">
+                    //altrimenti controllo se sta facendo login in post
+                } else if (isset($_POST['mail']) && isset($_POST['password'])) {
+                    $login = $account->login($_POST['mail'], $_POST['password']);
+
+                    if ($login) {
+                        echo '<br><div class="alert alert-success">
                         <strong>Login effettuato!</strong> Login effettuato con successo, verrai reindirizzato alla home.
                         </div>';
 
-                header('Refresh: 2; URL=/');
-                die();
-            } else {
-                echo '<br><div class="alert alert-danger fade in">
+                        header('Refresh: 2; URL=/');
+                        die();
+                    } else {
+                        echo '<br><div class="alert alert-danger">
                         <strong>Login fallito!</strong> Nome utente e/o password errati.
-                    </div>';
-            }
-        }
-    } catch (Exception $e) {
-        echo '<div class="alert alert-danger">
+                    </div><br>';
+                    }
+                }
+            } catch (Exception $e) {
+                echo '<br><div class="alert alert-danger">
                 <strong>Errore!</strong> Errore durante il login.
-            </div>';
-    }
-    ?>
+            </div><br>';
+            }
+            ?>
 
-    <!-- form di login -->
-    <center>
-        <div id="login">
-            <header>
-                <p>Login Utente</p>
-            </header>
-            <form id="form" action="login.php" method="post">
-                <input id="txt" type="email" placeholder="E-Mail" name="mail" required><br>
-                <input id="txt" type="password" placeholder="Password" name="password" required><br>
-                <hr>
-                <input type="submit" id="button" value="Login">
-            </form>
-        </div>
+            <a href="index.php"><img class="mb-4" src="img/logo.svg" alt="" width="150"></a>
+            <br>
 
-        <!-- link in caso non sia già registrato che rimanda a registrazione -->
-        Non sei ancora registrato? <a href="register.php">Registrati</a>
-        <br>
+            <h1 class="h3 mb-3 fw-normal">Login Utente</h1>
 
-    </center>
+            <div class="form-floating">
+                <input name="mail" type="email" class="form-control firstElement" id="floatingInput" placeholder="E-Mail" required>
+            </div>
+            <div class="form-floating">
+                <input name="password" type="password" class="form-control lastElement" id="floatingInput" placeholder="Password" required>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+            <a href="register.php" class="w-100 btn btn-lg btn-link" role="button">Crea un account</a>
+        </form>
+    </div>
 </body>
 
 </html>
