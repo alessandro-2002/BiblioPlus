9b9da85c3af57ca59f2a1b40572ca729221a0a6e
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-05-17 18:39:23 +0200

    Update header.php
    Update riferimenti js e css

--- assets/header.php
+++ assets/header.php
@@ -7,9 +7,13 @@ $fileName = basename($_SERVER['PHP_SELF']);
 <html>
 
 <head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 
