1e5e38aa2864f2558404c2d5476ea57f4083fc75
alessandro-2002 <58444175+alessandro-2002@users.noreply.github.com>
2021-05-20 22:27:50 +0200

    Update admin_new_book.php
    Aggiunta parte in POST per aggiunta libri.
    Manca la gestione della cover

--- admin/admin_new_book.php
+++ admin/admin_new_book.php
@@ -24,45 +24,259 @@
         if ($adminAccount->getACLcatalogue()) {
 
             //in caso ci sia inserimento in post aggiungo il titolo con transazione
+            if (isset($_POST['ISBN']) && isset($_POST['title']) && isset($_POST['publisher'])) {
 
-            /* DA FARE ANCORA */
-            /*
-                if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['mail']) && $adminAccount->getACLuser()) {
+                //blocco try-catch unico
+                try {
 
-                    //invoco la funzione per l'edit dell'utente
+                    /* controllo esistenza ISBN */
+
+                    //query di ricerca per libro esistente
+                    $query = "SELECT ISBN
+                                FROM book
+                                WHERE ISBN=:ISBN";
+
+                    //array di valori da passare
+                    $values = array(':ISBN' => $_POST['ISBN']);
+
+                    // esecuzione query 
                     try {
-                        //creo array per parametri opzionali indirizzo e enabled
-                        $options = array('address' => $_POST['address']);
+                        //prepare query
+                        $res = $pdo->prepare($query);
 
-                        if (isset($_POST['enabled']) && $_POST['enabled'] == "on") {
-                            $options['enabled'] = 1;
-                        } else {
-                            $options['enabled'] = 0;
+                        //esecuzione con passaggio di valori
+                        $res->execute($values);
+                    } catch (PDOException $e) {
+                        throw new Exception("Query error");
+                    }
+
+                    //controllo se non c'è libro, in tal caso lancio eccezione altrimenti continuo
+                    if ($res->rowCount() != 0) {
+                        throw new Exception("ISBN gi&agrave; presente nel catalogo.");
+                    }
+
+                    //inizio transazione per inserimenti
+                    try {
+                        $pdo->beginTransaction();
+
+                        /* controllo match con editore, in tal caso salvo id */
+
+                        //query di ricerca per editore esistente
+                        $query = "SELECT idPublisher
+                                FROM publisher
+                                WHERE name=:name";
+
+                        //array di valori da passare
+                        $values = array(':name' => $_POST['publisher']);
+
+                        // esecuzione query 
+                        try {
+                            //prepare query
+                            $res = $pdo->prepare($query);
+
+                            //esecuzione con passaggio di valori
+                            $res->execute($values);
+                        } catch (PDOException $e) {
+                            throw new Exception("Query error");
                         }
 
-                        $user->editAccount($user->getId(), $_POST['name'], $_POST['surname'], $_POST['mail'], $options);
+                        //controllo se c'è editore, in tal caso salvo id altrimenti lo inserisco
+                        if ($res->rowCount() == 1) {
+                            $idEditore = $res->fetchAll();
+                            $idEditore = $idEditore[0]['idPublisher'];
+                        } else {
+                            //query di inserimento per editore
+                            $query = "INSERT INTO publisher(name) VALUES(:name)";
 
-                        //stampo successo e ricarico pagina
+                            //array di valori da passare
+                            $values = array(':name' => $_POST['publisher']);
+
+                            // esecuzione query 
+                            try {
+                                //prepare query
+                                $res = $pdo->prepare($query);
+
+                                //esecuzione con passaggio di valori
+                                $res->execute($values);
+
+                                //salvo l'id dell'editore appena inserito
+                                $idEditore = $pdo->lastInsertId();
+                            } catch (PDOException $e) {
+                                throw new Exception("Query error");
+                            }
+                        }
+
+                        /* controllo match con autori e generazione array con id altrimenti li inserisco */
+                        $autori = array();
+
+                        //query base ricerca per autore esistente
+                        $query = "SELECT idAuthor
+                                FROM author
+                                WHERE name=:name";
+
+                        //foreach per scorrere array autori in post
+                        foreach ($_POST['authors'] as $author) {
+                            //array di valori da passare
+                            $values = array(':name' => $author);
+
+                            // esecuzione query 
+                            try {
+                                //prepare query
+                                $res = $pdo->prepare($query);
+
+                                //esecuzione con passaggio di valori
+                                $res->execute($values);
+                            } catch (PDOException $e) {
+                                throw new Exception("Query error");
+                            }
+
+                            //controllo se c'è autore, in tal caso salvo id altrimenti lo inserisco
+                            if ($res->rowCount() == 1) {
+                                $idAutore = $res->fetchAll();
+                                $autori[] = $idAutore[0]['idAuthor'];
+                            } else {
+                                //query di inserimento per autore
+                                $query = "INSERT INTO author(name) VALUES(:name)";
+
+                                //array di valori da passare
+                                $values = array(':name' => $author);
+
+                                // esecuzione query 
+                                try {
+                                    //prepare query
+                                    $res = $pdo->prepare($query);
+
+                                    //esecuzione con passaggio di valori
+                                    $res->execute($values);
+
+                                    //salvo l'id dell'autore appena inserito
+                                    $autori[] = $pdo->lastInsertId();
+                                } catch (PDOException $e) {
+                                    throw new Exception("Query error");
+                                }
+                            }
+                        }
+
+                        /* Inserimento del nuovo libro */
+
+                        //array di valori da passare
+                        $values = array(':ISBN' => $_POST['ISBN'], ':idPublisher' => $idEditore);
+
+                        //salvo tutti i parametri e gestisco i NULL
+                        if (isset($_POST['title'])) {
+                            $values[':title'] = $_POST['title'];
+                        } else {
+                            throw new Exception("Nessun titolo inserito.");
+                        }
+                        if (isset($_POST['subtitle']) && $_POST['subtitle'] != "") {
+                            $values[':subtitle'] = $_POST['subtitle'];
+                        } else {
+                            $values[':subtitle'] = NULL;
+                        }
+                        if (isset($_POST['language']) && $_POST['language'] != "") {
+                            $values[':language'] = $_POST['language'];
+                        } else {
+                            $values[':language'] = NULL;
+                        }
+                        if (isset($_POST['year']) && $_POST['year'] != "") {
+                            $values[':year'] = $_POST['year'];
+                        } else {
+                            $values[':year'] = NULL;
+                        }
+
+                        //gestione cover ora ci penso
+                        $values[':cover'] = NULL;
+
+                        //query di inserimento per libro
+                        $query = "INSERT INTO book(ISBN, title, subtitle, language, year, cover, idPublisher) 
+                                VALUES(:ISBN, :title, :subtitle, :language, :year, :cover, :idPublisher)";
+
+                        // esecuzione query 
+                        try {
+                            //prepare query
+                            $res = $pdo->prepare($query);
+
+                            //esecuzione con passaggio di valori
+                            $res->execute($values);
+                        } catch (PDOException $e) {
+                            throw new Exception("Query error " . $e->getMessage());
+                        }
+
+                        /* Inserimento nella nn write_book */
+
+                        //query di base inserimento autore, libro in nn
+                        $query = "INSERT INTO write_book(ISBN, idAuthor, position) 
+                                VALUES(:ISBN, :idAuthor, :position)";
+
+                        //array di valori da passare
+                        $values = array(':ISBN' => $_POST['ISBN']);
+
+                        //foreach per scorrere autori
+                        foreach ($autori as $index => $autore) {
+
+                            //inserimento idAutore in values da passare
+                            $values[':idAuthor'] = $autore;
+                            $values[':position'] = $index;
+
+                            // esecuzione query 
+                            try {
+                                //prepare query
+                                $res = $pdo->prepare($query);
+
+                                //esecuzione con passaggio di valori
+                                $res->execute($values);
+                            } catch (PDOException $e) {
+                                throw new Exception("Query error");
+                            }
+                        }
+
+                        /* inserimento n copie */
+                        //query di base inserimento copie
+                        $query = "INSERT INTO copy(ISBN) 
+                                VALUES(:ISBN)";
+
+                        //array di valori da passare
+                        $values = array(':ISBN' => $_POST['ISBN']);
+
+                        for ($i = 0; $i < $_POST['nCopie']; $i++) {
+                            // esecuzione query 
+                            try {
+                                //prepare query
+                                $res = $pdo->prepare($query);
+
+                                //esecuzione con passaggio di valori
+                                $res->execute($values);
+                            } catch (PDOException $e) {
+                                throw new Exception("Query error");
+                            }
+                        }
+
+
+                        //se tutti gli inserimenti sono andati a buon fine commit
+                        $pdo->commit();
+
+                        //stampo messaggio di successo
                         echo '<br><div class="alert alert-success">
-                        <strong>Modifica effettuata!</strong> Modifiche apportate con successo, la pagina verr&agrave; ricaricata.
-                    </div>';
+                                <strong>Libro inserito con successo!</strong> Verrai reindirizzato al riepilogo.' .
+                            '</div>';
 
-                        //in caso la modifica sia andata a buon fine la pagina viene ricaricata e poi die()
-                        header('Refresh: 3');
+                        header("Refresh:2; URL=admin_edit_book.php?ISBN=" . $_POST['ISBN']);
                         die();
-
-
-                        //in caso di errore, si stampa l'errore e si lascia stampare il resto della pagina
                     } catch (Exception $e) {
-                        echo '<div class="alert alert-danger">
-                        <strong>Errore!</strong> ' . $e->getMessage() . '
-                    </div>';
+                        // rollback se errore
+                        $pdo->rollback();
+                        throw new Exception($e);
                     }
-                }*/
+                } catch (Exception $e) {
+                    echo '<div class="alert alert-danger">
+                            <strong>Errore!</strong> ' . $e->getMessage() . '
+                        </div>';
+                }
+            }
 
         ?>
 
-            <!-- VISTA USER -->
+            <!-- VISTA LIBRO -->
             <div class="container">
                 <h1>Nuovo Libro</h1>
 
@@ -131,7 +345,7 @@
                         <label class="col-4 col-form-label">Editore:</label>
                         <div class="col-8">
                             <div class="input-group">
-                                <input class="form-control" type="year" name="year" id="publisher">
+                                <input class="form-control" type="text" name="publisher" id="publisher" required>
                             </div>
                         </div>
                     </div>
